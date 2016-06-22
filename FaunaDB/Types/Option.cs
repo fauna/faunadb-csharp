5a69ced0891d577b0beb916349ca4ea712987708
Marrony Neris <marrony@faunadb.com>
2016-06-22 15:40:17 -0700

    adds Option class

---
+++ FaunaDB/Types/Option.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace FaunaDB.Types
+{
+    public interface IOption
+    {
+        bool HasValue { get; }
+    }
+
+    public class Option<T> : IOption
+    {
+        private T _value;
+        public T Value
+        {
+            private set
+            {
+                _value = value;
+            }
+            get
+            {
+                if (!HasValue)
+                    throw new ArgumentNullException("");
+
+                return _value;
+            }
+        }
+
+        public bool HasValue { get; private set; }
+
+        internal Option(T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            Value = value;
+            HasValue = true;
+        }
+
+        internal Option()
+        {
+            Value = default(T);
+            HasValue = false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            IOption other0 = obj as IOption;
+            if (!HasValue && !other0.HasValue)
+                return true;
+
+            Option<T> other1 = obj as Option<T>;
+            return other1 != null && HasValue == other1.HasValue && Equals(Value, other1.Value);
+        }
+
+        public override int GetHashCode() =>
+            HasValue ? Value.GetHashCode() : 0;
+
+        public override string ToString() =>
+            HasValue ? $"Some({Value.ToString()})" : "None()";
+    }
+
+    public class Option
+    {
+        public static Option<T> Some<T>(T value) =>
+            new Option<T>(value);
+
+        public static Option<T> None<T>() =>
+            new Option<T>();
+    }
+}
