1a6d58c420af8b6ab88df963dbcbadb1d1b91d9a
Marrony Neris <marrony@faunadb.com>
2016-06-28 07:27:49 -0700

    remove unused class and imports

--- FaunaDB/Query/Language.Basic.cs
+++ FaunaDB/Query/Language.Basic.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FaunaDB.Query
+﻿namespace FaunaDB.Query
 {
     public partial struct Language
     {
