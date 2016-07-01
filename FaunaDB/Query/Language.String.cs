e8cae52300c6fcab6404bd37816fd0e3bab9d39a
Marrony Neris <marrony@faunadb.com>
2016-07-01 17:15:32 -0300

    merging Concat functions

--- FaunaDB/Query/Language.String.cs
+++ FaunaDB/Query/Language.String.cs
@@ -7,13 +7,7 @@
         /// <summary>
         /// See the <see href="https://faunadb.com/documentation/queries#string_functions">docs</see>. 
         /// </summary>
-        public static Expr Concat(Expr strings) =>
-            UnescapedObject.With("concat", strings);
-
-        /// <summary>
-        /// See the <see href="https://faunadb.com/documentation/queries#string_functions">docs</see>. 
-        /// </summary>
-        public static Expr Concat(Expr strings, Expr separator) =>
+        public static Expr Concat(Expr strings, Expr separator = null) =>
             UnescapedObject.With("concat", strings, "separator", separator);
 
         /// <summary>
