37b258063318c23bfd1bfe96e22f1203b746a46f
Marrony Neris <marrony@faunadb.com>
2016-07-11 19:50:47 -0300

    document collection functions

--- FaunaDB/Query/Language.Collection.Foreach.cs
+++ FaunaDB/Query/Language.Collection.Foreach.cs
@@ -4,21 +4,69 @@ namespace FaunaDB.Query
 {
     public partial struct Language
     {
+        /// <summary>
+        /// Creates a new Foreach expression with a lambda function that receives one argument.
+        /// <para>
+        /// <see href="https://faunadb.com/documentation/queries#collection_functions">FaunaDB Collection Functions</see>
+        /// </para>
+        /// </summary>
+        /// <param name="collection">A collection expression</param>
+        /// <param name="lambda">A lambda function that receives one argument</param>
         public static Expr Foreach(Expr collection, Func<Expr, Expr> lambda) =>
             Foreach(collection, Lambda(lambda));
 
+        /// <summary>
+        /// Creates a new Foreach expression with a lambda function that receives two arguments.
+        /// <para>
+        /// <see href="https://faunadb.com/documentation/queries#collection_functions">FaunaDB Collection Functions</see>
+        /// </para>
+        /// </summary>
+        /// <param name="collection">A collection expression</param>
+        /// <param name="lambda">A lambda function that receives two arguments</param>
         public static Expr Foreach(Expr collection, Func<Expr, Expr, Expr> lambda) =>
             Foreach(collection, Lambda(lambda));
 
+        /// <summary>
+        /// Creates a new Foreach expression with a lambda function that receives three arguments.
+        /// <para>
+        /// <see href="https://faunadb.com/documentation/queries#collection_functions">FaunaDB Collection Functions</see>
+        /// </para>
+        /// </summary>
+        /// <param name="collection">A collection expression</param>
+        /// <param name="lambda">A lambda function that receives three arguments</param>
         public static Expr Foreach(Expr collection, Func<Expr, Expr, Expr, Expr> lambda) =>
             Foreach(collection, Lambda(lambda));
 
+        /// <summary>
+        /// Creates a new Foreach expression with a lambda function that receives four arguments.
+        /// <para>
+        /// <see href="https://faunadb.com/documentation/queries#collection_functions">FaunaDB Collection Functions</see>
+        /// </para>
+        /// </summary>
+        /// <param name="collection">A collection expression</param>
+        /// <param name="lambda">A lambda function that receives four arguments</param>
         public static Expr Foreach(Expr collection, Func<Expr, Expr, Expr, Expr, Expr> lambda) =>
             Foreach(collection, Lambda(lambda));
 
+        /// <summary>
+        /// Creates a new Foreach expression with a lambda function that receives five arguments.
+        /// <para>
+        /// <see href="https://faunadb.com/documentation/queries#collection_functions">FaunaDB Collection Functions</see>
+        /// </para>
+        /// </summary>
+        /// <param name="collection">A collection expression</param>
+        /// <param name="lambda">A lambda function that receives five arguments</param>
         public static Expr Foreach(Expr collection, Func<Expr, Expr, Expr, Expr, Expr, Expr> lambda) =>
             Foreach(collection, Lambda(lambda));
 
+        /// <summary>
+        /// Creates a new Foreach expression with a lambda function that receives six arguments.
+        /// <para>
+        /// <see href="https://faunadb.com/documentation/queries#collection_functions">FaunaDB Collection Functions</see>
+        /// </para>
+        /// </summary>
+        /// <param name="collection">A collection expression</param>
+        /// <param name="lambda">A lambda function that receives six arguments</param>
         public static Expr Foreach(Expr collection, Func<Expr, Expr, Expr, Expr, Expr, Expr, Expr> lambda) =>
             Foreach(collection, Lambda(lambda));
     }
