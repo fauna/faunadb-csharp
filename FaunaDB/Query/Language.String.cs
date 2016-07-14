8dbb437c2770be9b4905dcc30ffe075c0306567b
Marrony Neris <marrony@faunadb.com>
2016-07-14 12:49:35 -0300

    document string functions

--- FaunaDB/Query/Language.String.cs
+++ FaunaDB/Query/Language.String.cs
@@ -3,13 +3,19 @@
     public partial struct Language
     {
         /// <summary>
-        /// See the <see href="https://faunadb.com/documentation/queries#string_functions">docs</see>.
+        /// Creates a new Concat expression.
+        /// <para>
+        /// See the <see href="https://faunadb.com/documentation/queries#string_functions">FaunaDB String Functions</see>
+        /// </para>
         /// </summary>
         public static Expr Concat(Expr strings, Expr separator = null) =>
             UnescapedObject.With("concat", strings, "separator", separator);
 
         /// <summary>
-        /// See the <see href="https://faunadb.com/documentation/queries#string_functions">docs</see>.
+        /// Creates a new CaseFold expression.
+        /// <para>
+        /// See the <see href="https://faunadb.com/documentation/queries#string_functions">FaunaDB String Functions</see>
+        /// </para>
         /// </summary>
         public static Expr Casefold(Expr @string) =>
             UnescapedObject.With("casefold", @string);
