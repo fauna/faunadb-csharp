4f2bcc176e70ac966a1786277cf34433aa84b2e7
Marrony Neris <marrony@faunadb.com>
2016-07-27 13:08:11 -0300

    adds pagination helper

---
+++ FaunaDB/Query/PageHelper.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FaunaDB.Types;
+
+namespace FaunaDB.Query
+{
+    public class PageHelper
+    {
+        readonly Client.Client client;
+        readonly Expr set;
+        readonly Expr ts;
+        Expr after;
+        Expr before;
+        readonly Expr size;
+        readonly Expr events;
+        readonly Expr sources;
+        readonly List<Func<Expr, Expr>> faunaFunctions;
+
+        public PageHelper(Client.Client client,
+                          Expr set,
+                          Expr ts = null,
+                          Expr after = null,
+                          Expr before = null,
+                          Expr size = null,
+                          Expr events = null,
+                          Expr sources = null)
+        {
+            this.client = client;
+            this.set = set;
+            this.ts = ts;
+            this.after = after;
+            this.before = before;
+            this.size = size;
+            this.events = events;
+            this.sources = sources;
+            this.faunaFunctions = new List<Func<Expr, Expr>>();
+        }
+
+        public PageHelper Map(Expr lambda)
+        {
+            faunaFunctions.Add((q) => Language.Map(q, lambda));
+            return this;
+        }
+
+        public PageHelper Filter(Expr lambda)
+        {
+            faunaFunctions.Add((q) => Language.Filter(q, lambda));
+            return this;
+        }
+
+        public async Task Each(Action<Value> lambda)
+        {
+            await RetrieveNextPage(after, false)
+                .ContinueWith(ConsumePage(lambda, false))
+                .Unwrap();
+        }
+
+        public async Task EachReverse(Action<Value> lambda)
+        {
+            await RetrieveNextPage(before, true)
+                .ContinueWith(ConsumePage(lambda, true))
+                .Unwrap();
+        }
+
+        public async Task<Value> NextPage()
+        {
+            return await RetrieveNextPage(after, false)
+                .ContinueWith(AdjustCursors);
+        }
+
+        public async Task<Value> PreviousPage()
+        {
+            return await RetrieveNextPage(before, true)
+                .ContinueWith(AdjustCursors);
+        }
+
+        Value AdjustCursors(Task<Value> page)
+        {
+            var result = page.Result;
+
+            if (result.At("after") != NullV.Instance)
+                after = result.At("after");
+
+            if (result.At("before") != NullV.Instance)
+                before = result.At("before");
+
+            return result.At("data");
+        }
+
+        Func<Task<Value>, Task<Value>> ConsumePage(Action<Value> lambda, bool reverse)
+        {
+            return (task) => {
+                var page = task.Result;
+                var data = page.At("data");
+
+                lambda(data);
+
+                Expr nextCursor = reverse ? page.At("before") : page.At("after");
+
+                if (nextCursor != NullV.Instance)
+                {
+                    return RetrieveNextPage(nextCursor, reverse)
+                        .ContinueWith(ConsumePage(lambda, reverse))
+                        .Unwrap();
+                }
+
+                return Task.FromResult(NullV.Instance);
+            };
+        }
+
+        Task<Value> RetrieveNextPage(Expr cursor, bool reverse)
+        {
+            Expr _after = null;
+            Expr _before = null;
+
+            if (cursor != null)
+            {
+                if (reverse)
+                    _before = cursor;
+                else
+                    _after = cursor;
+            }
+
+            var q = Language.Paginate(set, ts, _after, _before, size, events, sources);
+
+            foreach (var lambda in faunaFunctions)
+                q = lambda(q);
+
+            return client.Query(q);
+        }
+    }
+}
+
---
+++ Test/PageTest.cs
@@ -0,0 +1,332 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaunaDB.Query;
+using FaunaDB.Types;
+using NUnit.Framework;
+
+using static FaunaDB.Query.Language;
+
+namespace Test
+{
+    [TestFixture]
+    public class PageTest : TestCase
+    {
+        Expr classRef, indexRef;
+        Dictionary<long, Value> instanceRefs = new Dictionary<long, Value>();
+        Dictionary<Value, long> refsToIndex = new Dictionary<Value, long>();
+
+        Expr tsClassRef, tsIndexRef, tsInstance1Ref, tsInstance1Ts;
+
+        [OneTimeSetUp]
+        new public void SetUp()
+        {
+            SetUpAsync().Wait();
+        }
+
+        async Task SetUpAsync()
+        {
+            await SetupTimestampedThings();
+            await SetupPagedThings();
+        }
+
+        async Task SetupTimestampedThings()
+        {
+            var task = await client.Query(Create(Ref("classes"), Obj("name", "timestamped_things")));
+            tsClassRef = task.At("ref");
+
+            var task1 = await client.Query(Create(Ref("indexes"), Obj(
+                    "name", "timestamped_things_by_class",
+                    "source", tsClassRef
+            )));
+            tsIndexRef = task1.At("ref");
+
+            var task2 = await client.Query(Create(tsClassRef, Obj()));
+            tsInstance1Ref = task2.At("ref");
+            tsInstance1Ts = task2.At("ts");
+
+            await client.Query(Create(tsClassRef, Obj()));
+        }
+
+        async Task SetupPagedThings()
+        {
+            var task = await client.Query(Create(Ref("classes"), Obj("name", "paged_things")));
+            classRef = task.At("ref");
+
+            var task2 = await client.Query(Create(Ref("indexes"), Obj(
+                    "name", "things_by_class",
+                    "source", classRef,
+                    "values", Arr(Obj("field", Arr("data", "i")), Obj("field", "ref"))
+            )));
+            indexRef = task2.At("ref");
+
+            for (int i = 0; i < 100; i++)
+            {
+                var result = await client.Query(Create(classRef, Obj("data", Obj("i", i))));
+                var index = result.At("data", "i").To(Codec.LONG).Value;
+
+                instanceRefs.Add(index, result.At("ref"));
+                refsToIndex.Add(result.At("ref"), index);
+            }
+        }
+
+        [Test]
+        public async Task TestPagination()
+        {
+            var page = new PageHelper(client, Match(indexRef));
+
+            await page.Each(p =>
+            {
+                var array = p as ArrayV;
+
+                foreach (var value in array)
+                {
+                    var i = value.At(0).To(Codec.LONG).Value;
+                    var r = value.At(1);
+
+                    Assert.AreEqual(instanceRefs[i], r);
+                }
+            });
+        }
+
+        [Test]
+        public async Task TestReversePagination()
+        {
+            var page = new PageHelper(client, Match(indexRef), before: NullV.Instance);
+
+            var item = 100;
+            await page.EachReverse(p =>
+            {
+                var array = (p as ArrayV).Reverse();
+
+                foreach (var value in array)
+                {
+                    var @ref = value.At(1);
+
+                    Assert.AreEqual(item - 1, refsToIndex[@ref]);
+
+                    item--;
+                }
+            });
+
+            Assert.AreEqual(0, item);
+        }
+
+        [Test]
+        public async Task MapsPagination()
+        {
+            var page = new PageHelper(client, Match(indexRef));
+
+            var pageMapped = page.Map(Lambda(obj => Select(Path(1), obj)));
+
+            var item = 0;
+            await pageMapped.Each(p => {
+                var array = p as ArrayV;
+
+                foreach (var value in array)
+                {
+                    Assert.AreEqual(item, refsToIndex[value]);
+
+                    item++;
+                }
+            });
+
+            Assert.AreEqual(100, item);
+        }
+
+        [Test]
+        public async Task FilterPagination()
+        {
+            var page = new PageHelper(client, Match(indexRef));
+
+            var pageFiltered = page.Filter(Lambda(obj => {
+                return EqualsFn(Modulo(Select(Path(0), obj), 2), 0);
+            }));
+
+            var item = 0;
+            await pageFiltered.Each(p =>
+            {
+                var array = p as ArrayV;
+
+                foreach (var value in array)
+                {
+                    Assert.AreEqual(item, refsToIndex[value.At(1)]);
+
+                    item += 2;
+                }
+            });
+
+            Assert.AreEqual(100, item);
+        }
+
+        [Test]
+        public async Task HonorsPassedInCursor()
+        {
+            var page = new PageHelper(client, Match(indexRef), after: 50);
+
+            var item = 50;
+            await page.Each(p => {
+                var array = p as ArrayV;
+
+                foreach (var value in array)
+                {
+                    Assert.AreEqual(item, refsToIndex[value.At(1)]);
+
+                    item++;
+                }
+            });
+
+            Assert.AreEqual(item, 100);
+        }
+
+        [Test]
+        public async Task HonorsPassedInCursorInTheReverseDirection()
+        {
+            var page = new PageHelper(client, Match(indexRef), before: 51);
+
+            var item = 51;
+            await page.EachReverse(p => {
+                var array = (p as ArrayV).Reverse();
+
+                foreach (var value in array)
+                {
+                    Assert.AreEqual(item - 1, refsToIndex[value.At(1)]);
+
+                    item--;
+                }
+            });
+
+            Assert.AreEqual(item, 0);
+        }
+
+        [Test]
+        public async Task HonorsSize()
+        {
+            var numPages = 20;
+            var pageSize = 100 / numPages;
+
+            var page = new PageHelper(client, Match(indexRef), size: pageSize);
+
+            var item = 0;
+            await page.Each(p => {
+                var array = p as ArrayV;
+
+                Assert.AreEqual(array.Length, pageSize);
+
+                item++;
+            });
+
+            Assert.AreEqual(item, numPages);
+        }
+
+        [Test]
+        public async Task HonorsTs()
+        {
+            var page = new PageHelper(client, Match(tsIndexRef));
+
+            await page.Each(item => {
+                Assert.That(item, Has.Length.EqualTo(2));
+            });
+
+            var page2 = new PageHelper(client, Match(tsIndexRef), ts: tsInstance1Ts);
+            await page2.Each(item => {
+                var array = item as ArrayV;
+
+                Assert.That(array, Has.Length.EqualTo(1));
+                Assert.AreEqual(tsInstance1Ref, array[0]);
+            });
+        }
+
+        [Test]
+        public async Task HonorsEvents()
+        {
+            var page = new PageHelper(client, Match(indexRef), events: true);
+
+            await page.Each(item => {
+                var array = item as ArrayV;
+
+                foreach (var value in array)
+                {
+                    Assert.AreNotSame(value.At("ts"), NullV.Instance);
+                    Assert.AreNotSame(value.At("action"), NullV.Instance);
+                    Assert.AreNotSame(value.At("resource"), NullV.Instance);
+                    Assert.AreNotSame(value.At("values"), NullV.Instance);
+                }
+            });
+        }
+
+        [Test]
+        public async Task HornosSource()
+        {
+            var page = new PageHelper(client, Match(indexRef), sources: true);
+
+            await page.Each(item => {
+                var array = item as ArrayV;
+
+                foreach (var value in array)
+                {
+                    Assert.AreNotSame(value.At("sources"), NullV.Instance);
+                }
+            });
+        }
+
+        [Test]
+        public async Task HornosCombinationParameters()
+        {
+            var page = new PageHelper(client, Match(indexRef), before: NullV.Instance, events: true, sources: true);
+
+            await page.Each(item => {
+                var array = item as ArrayV;
+
+                foreach (var value in array)
+                {
+                    Assert.AreNotSame(value.At("value"), NullV.Instance);
+                    Assert.AreNotSame(value.At("sources"), NullV.Instance);
+
+                    Assert.AreNotSame(value.At("value", "ts"), NullV.Instance);
+                    Assert.AreNotSame(value.At("value", "action"), NullV.Instance);
+                    Assert.AreNotSame(value.At("value", "resource"), NullV.Instance);
+                    Assert.AreNotSame(value.At("value", "values"), NullV.Instance);
+                    
+                }
+            });
+        }
+
+        [Test]
+        public async Task IterativelyPaginatePages()
+        {
+            var page = new PageHelper(client, Match(indexRef), size: 2);
+
+            var item1 = await page.NextPage();
+
+            Assert.That(item1, Has.Length.EqualTo(2));
+            Assert.AreEqual(0, refsToIndex[item1.At(0, 1)]);
+            Assert.AreEqual(1, refsToIndex[item1.At(1, 1)]);
+
+            var item2 = await page.NextPage();
+
+            Assert.That(item2, Has.Length.EqualTo(2));
+            Assert.AreEqual(2, refsToIndex[item2.At(0, 1)]);
+            Assert.AreEqual(3, refsToIndex[item2.At(1, 1)]);
+        }
+
+        [Test]
+        public async Task IterativelyPaginatePagesInReverseDirection()
+        {
+            var page = new PageHelper(client, Match(indexRef), before: NullV.Instance, size: 2);
+
+            var item1 = await page.PreviousPage();
+
+            Assert.That(item1, Has.Length.EqualTo(2));
+            Assert.AreEqual(98, refsToIndex[item1.At(0, 1)]);
+            Assert.AreEqual(99, refsToIndex[item1.At(1, 1)]);
+
+            var item2 = await page.PreviousPage();
+
+            Assert.That(item2, Has.Length.EqualTo(2));
+            Assert.AreEqual(96, refsToIndex[item2.At(0, 1)]);
+            Assert.AreEqual(97, refsToIndex[item2.At(1, 1)]);
+        }
+    }
+}
+
