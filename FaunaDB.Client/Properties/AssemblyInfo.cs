ac395f40eb1f06c23a1bdbe5ab275f3c90d64178
Marrony Neris <marrony@faunadb.com>
2016-10-28 12:01:01 -0700

    releasing 0.1.2

--- FaunaDB.Client/Properties/AssemblyInfo.cs
+++ FaunaDB.Client/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@
 
 [assembly: AssemblyVersion(FaunaDBAttribute.Version)]
 [assembly: AssemblyFileVersion(FaunaDBAttribute.Version)]
-[assembly: AssemblyInformationalVersion(FaunaDBAttribute.Version + "-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion(FaunaDBAttribute.Version)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
