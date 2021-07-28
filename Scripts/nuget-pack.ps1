3bc7b9fe7cdaa666a343ed2329eb540a265b5617
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-07-28 12:54:39 +0300

    chore(FE-1444): Clean-up of the unused resources

--- Scripts/nuget-pack.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item packages\FaunaDB.Client -Recurse -Force
-New-Item packages\FaunaDB.Client -type directory
-nuget.exe pack .\FaunaDB.Client\FaunaDB.Client.csproj -OutputDirectory packages\FaunaDB.Client -Properties Configuration=Release
