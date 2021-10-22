00acb7d68fff8a2e0ebad505f95940d33267bf80
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-10-22 16:11:13 +0300

    chore: returned changes needed for testing

--- concourse/scripts/publish.sh
+++ concourse/scripts/publish.sh
@@ -7,12 +7,11 @@ cd ./fauna-csharp-repository
 apk add xmlstarlet
 PACKAGE_VERSION=$(xml sel -t -v "/Project/PropertyGroup/Version" ./FaunaDB.Client/FaunaDB.Client.csproj)
 
-# dotnet clean
-# dotnet restore
-# dotnet build ./FaunaDB.Client --configuration Release
-# dotnet pack ./FaunaDB.Client/FaunaDB.Client.csproj --no-build --no-restore --include-symbols -p:SymbolPackageFormat=snupkg --include-source --configuration Release
-# 
-# dotnet nuget push ./FaunaDB.Client/bin/Release/*.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
+dotnet clean
+dotnet restore
+dotnet build ./FaunaDB.Client --configuration Release
+dotnet pack ./FaunaDB.Client/FaunaDB.Client.csproj --no-build --no-restore --include-symbols -p:SymbolPackageFormat=snupkg --include-source --configuration Release
 
-# echo "faunadb-go@$PACKAGE_VERSION has been released [fyi <@ewan.edwards>]" > ../slack-message/publish
-echo "faunadb-go@$PACKAGE_VERSION test (please ignore this message)" > ../slack-message/publish
+dotnet nuget push ./FaunaDB.Client/bin/Release/*.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
+
+echo "faunadb-csharp@$PACKAGE_VERSION has been published into nuget [fyi <@ewan.edwards>]" > ../slack-message/publish
