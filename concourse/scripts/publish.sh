7795746d239158031dae02687a54d0c3f9661875
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-10-22 16:22:04 +0300

    FE-1409: Adds slack notification for release pipeline (#174)

--- concourse/scripts/publish.sh
+++ concourse/scripts/publish.sh
@@ -4,9 +4,14 @@ set -eou
 
 cd ./fauna-csharp-repository
 
+apk add xmlstarlet
+PACKAGE_VERSION=$(xml sel -t -v "/Project/PropertyGroup/Version" ./FaunaDB.Client/FaunaDB.Client.csproj)
+
 dotnet clean
 dotnet restore
 dotnet build ./FaunaDB.Client --configuration Release
 dotnet pack ./FaunaDB.Client/FaunaDB.Client.csproj --no-build --no-restore --include-symbols -p:SymbolPackageFormat=snupkg --include-source --configuration Release
 
 dotnet nuget push ./FaunaDB.Client/bin/Release/*.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
+
+echo "faunadb-csharp@$PACKAGE_VERSION has been published into nuget [fyi <@ewan.edwards>]" > ../slack-message/publish
